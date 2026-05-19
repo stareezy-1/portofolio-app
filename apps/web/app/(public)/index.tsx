b76c85999fac0c30cbf7351a6fab558383bf05a6
bintangakbarRK <bintang@rekosistem.com>
2026-05-20 00:57:16 +0700

    update icon

--- apps/web/app/(public)/index.tsx
+++ apps/web/app/(public)/index.tsx
@@ -193,6 +193,8 @@ export default function LandingPage() {
         />
         <meta name="theme-color" content={aurora.deepSpace.value} />
         <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/icon-192.svg" />
       </Head>
 
       {/* Hero */}
